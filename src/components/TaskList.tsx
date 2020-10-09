2042840c353916e5516babd92ce7e075b6f538f0
Stephano Lioe <stephanolioe@gmail.com>
2020-10-09 10:47:38 +0200

    minor

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import TaskItem from "./TaskItem";
-// import projects from "../data/projects.json";
-// import tags from "../data/tags.json";
 import tasks from "../data/tasks.json";
 
 import { useProjectState } from "../context/projectProvider";
@@ -29,7 +27,7 @@ const TaskList: React.FC = () => {
                 <span className="task-info text-xs text-gray-600 font-semibold">
                   4 resterernd
                 </span>
-                <span className="inline-block ml-1 w-1 h-1 bg-gray-600 rounded-full"></span>
+                <span className="inline-block ml-1 w-1 h-1 bg-gray-600 rounded-full" />
                 <span className="task-info text-xs text-red-900 pt-0 font-semibold bg-red-300 rounded ml-1 px-1">
                   4 achterstallig
                 </span>
